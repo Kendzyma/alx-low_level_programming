b8a56d20ce21c0d3e28136c0f50cbe3830d6236d
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-10 12:26:44 -0800

    dwdw

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This is the main unction
+ *
+ * Return: This unction returns zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
