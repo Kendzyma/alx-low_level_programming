a583906da5010d86ab47952401f8700b24ac9fab
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-10 12:15:33 -0800

    dwdw

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * main - This funtion prints a
+ *
+ * Return: intiger
+ */
 int main(void)
 {
 	char alx[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(alx);
 	return (0);
 }
