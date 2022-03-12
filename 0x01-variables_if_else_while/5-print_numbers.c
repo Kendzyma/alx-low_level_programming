c1f9cfa6303e879bbb4b6f52221b4230f27a0741
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-12 15:18:18 -0800

    dwdw

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is main
+ *
+ * Return: This returns
+ */
+int main(void)
+{
+	int firsNumber = 0;
+
+	while (firstNumber <= 9)
+	{
+		printf("%d", firstNumber);
+		firstNumber++;
+	}
+	print
+}
