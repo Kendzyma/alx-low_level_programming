87eb3d4e52692e7a7ea9fe5e7b186cd91efd4f19
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-12 13:42:24 -0800

    dwdw

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ *
+ * Return: This function returns success
+ */
+int main(void)
+{
+	for (char a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
