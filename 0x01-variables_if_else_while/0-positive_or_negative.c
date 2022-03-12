e64d984abf5b2d707e868a2cdc1f2819dfe0f517
Kendzyma <Kendzyma75@gmail.com>
2022-03-12 01:40:30 -0800

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,21 +8,23 @@
  */
 int main(void)
 {	
+	int n;
+
 	srand(time(0));
-	int n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n < 0)
 	{
-		printf("%d: is negative", n);
+		printf("%d: is negative\n", n);
 	}
 
 	else if (n > 0)
 	{
-		printf("%d: is postive", n);
+		printf("%d: is postive\n", n);
 	}
 	else
 	{
-		printf("%d: is zero", n);
+		printf("%d: is zero\n", n);
 	}
 	return (0);
 }
