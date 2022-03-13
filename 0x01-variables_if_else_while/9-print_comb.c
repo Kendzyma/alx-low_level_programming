d1c1d7577ef2a7289e8cde84c82931b76caa6feb
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-12 16:17:15 -0800

    dwdw

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: This returns
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		putchar(a + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
