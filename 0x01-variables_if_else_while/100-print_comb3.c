cf12418c6dafe18c689241ba8a16dedcd6d8fd76
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-14 02:06:53 -0700

    dwdw

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: This returns
+ */
+int main(void)
+{
+	int firstDigit = 0;
+
+	while (firstDigit <= 9)
+	{
+		int secondDigit = firstDigit + 1;
+		while (secondDigit)
+		{
+			putchar(firstDigit + '0');
+			putchar(secondDigit + '0');
+
+			if (!(firstDigit == 8 && secondDigit == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		secondDigit++;
+	}
+	putchar('\n');
+	return (0);
+}
