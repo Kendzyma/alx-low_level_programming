ff6933cff0e6c137dc1550505b0803d805833957
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-16 12:33:53 -0700

    dwdw

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 alphabets
+ *
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
