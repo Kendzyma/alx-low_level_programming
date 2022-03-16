1eb6f68f8f9e1905431b55378f6d4bfd8e01fe47
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-16 13:48:32 -0700

    dww

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - This is a sign
+ * @n: This is the parameter
+ *
+ * Returns: This returns type
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0)
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
