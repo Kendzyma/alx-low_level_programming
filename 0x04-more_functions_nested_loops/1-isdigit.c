7a989181a91d8d34da7f525a1d1a5838027b927b
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-17 12:23:09 -0700

    dwwdw

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isdigit(int c) - Yhea
+ * @c: Year
+ *
+ * Return: nothing
+ */
+int _isdigit(int c);
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
