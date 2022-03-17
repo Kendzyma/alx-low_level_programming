372cd6c6cbbcc2827b01a8387b4056acfd6583f3
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-17 12:43:52 -0700

    dwd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - Yes this is digit
+ * @a: yes o
+ * @b: yead
+ *
+ * Return: Returns a value
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
