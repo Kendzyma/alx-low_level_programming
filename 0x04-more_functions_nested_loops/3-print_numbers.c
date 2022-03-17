8d351b64efaa68a241afa914fb289122c3f05d8f
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-17 12:53:26 -0700

    dwdw

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Yea print smtg
+ *
+ * Return: The is goof
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
