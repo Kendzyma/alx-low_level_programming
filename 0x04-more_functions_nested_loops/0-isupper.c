34da710abe0aa4f665e6fd1c46f4b6186b1313c0
Tiamiyu Kehinde <Kendzyma75@gmail.com>
2022-03-17 12:08:26 -0700

    dwdw

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - This is upper
+ * @c: Input parameter
+ *
+ * Return: yes
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
