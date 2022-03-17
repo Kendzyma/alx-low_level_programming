1428bbf05ef72d5cb9dbda653a599c78a2943c2c
Tiamiyu Kehinde <Tiamiyukehinde5@gmail.com>
2022-03-17 21:57:19 +0100

    cc

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,13 @@
 #include "main.h"
-
 /**
-<<<<<<< HEAD
  * print_most_numbers - prints numbers, from 0 to 9 except 2 and 4
  * Return: void
-=======
- * print_most_numbers - hello
- *
- * Return: yes return
->>>>>>> 8511431822bbd38a493fb2ea46a6f7ea6ef7c1a3
  */
 
 void print_most_numbers(void)
 {
 	char c = '0';
 
-<<<<<<< HEAD
 	while (c <= '9')
 	{
 		if (c != '2' && c != '4')
@@ -26,14 +18,3 @@ void print_most_numbers(void)
 	}
 	_putchar('\n');
 }
-=======
-	while(i <= 0 )
-	{
-		if(!(i == 2 || i == 4))
-		{
-			_putchar(c);
-		}
-		i++;
-	}
-}
->>>>>>> 8511431822bbd38a493fb2ea46a6f7ea6ef7c1a3
