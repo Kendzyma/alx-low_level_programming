fa8da11e3158f378d48c128be2a80feb25145a47
Tiamiyu Kehinde <Tiamiyukehinde5@gmail.com>
2022-04-05 23:06:03 +0100

    dwd

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main - prints the number of arguments passed into it.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+
+int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
-	return (0);
+(void) argv; /*Ignore argv*/
+printf("%i\n", argc - 1);
+
+return (0);
 }
